63f0aaa88501ebd13fde369e42a9e046492b4416
Rony Boscan <ronyboscan@gmail.com>
2021-04-07 15:52:38 -0400

    final commit to publish

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  clientID: "476b7513192847849cdabf0bcd929acc",
+  clientSecret: "5b80dab17bda456685479ff04a6716a8",
+  userAPIBase: "https://web422-spotify-api.herokuapp.com/api/user"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
